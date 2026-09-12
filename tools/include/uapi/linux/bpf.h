yassshhhh15/android_kernel_realme_sm8250

BACKPORT: bpf: add for_each_map_elem helper ABI
Expose the callback helper in the kernel and tools UAPI and connect its
prototype to the existing helper dispatch and tracing metadata.  The verifier
callback state is supplied by the preceding commit.

Based on upstream commit:
69c087ba6225b574afb6e505b72cb75242a3d844 bpf: Add bpf_for_each_map_elem() helper

Manual adaptation: retain the 4.19 helper table and BTF prototype conventions
instead of replacing the vendor dispatch tables with their v5.15 versions.

Signed-off-by: Bruce Teng <torai.d.l@outlook.com>

tools/include/uapi/linux/bpf.h
@@ -390,6 +390,15 @@ enum bpf_iter_link_info {
  *                   is struct/union.
  */
 #define BPF_PSEUDO_BTF_ID	3
+/* insn[0].src_reg:  BPF_PSEUDO_FUNC
+ * insn[0].imm:      insn offset to the func
+ * insn[1].imm:      subprog number, filled in by the verifier
+ * insn[0].off:      0
+ * insn[1].off:      0
+ * ldimm64 rewrite:  address of the function
+ * verifier type:    PTR_TO_FUNC.
+ */
+#define BPF_PSEUDO_FUNC		4
 
 /* when bpf_call->src_reg == BPF_PSEUDO_CALL, bpf_call->imm == pc-relative
  * offset to another bpf function
@@ -3892,6 +3901,7 @@ union bpf_attr {
 	FN(task_storage_get),		\
 	FN(task_storage_delete),	\
 	FN(get_current_task_btf),	\
+	FN(for_each_map_elem),		\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper

yuksk-cyber/android_kernel_samsung_mt6768

tools/include/uapi/linux/bpf.h
@@ -389,6 +389,15 @@ enum bpf_iter_link_info {
  *                   is struct/union.
  */
 #define BPF_PSEUDO_BTF_ID	3
+/* insn[0].src_reg:  BPF_PSEUDO_FUNC
+ * insn[0].imm:      insn offset to the func
+ * insn[1].imm:      subprog number, filled in by the verifier
+ * insn[0].off:      0
+ * insn[1].off:      0
+ * ldimm64 rewrite:  address of the function
+ * verifier type:    PTR_TO_FUNC.
+ */
+#define BPF_PSEUDO_FUNC		4
 
 /* when bpf_call->src_reg == BPF_PSEUDO_CALL, bpf_call->imm == pc-relative
  * offset to another bpf function
@@ -3844,7 +3853,10 @@ union bpf_attr {
 	FN(per_cpu_ptr),		\
 	FN(this_cpu_ptr),		\
 	FN(redirect_peer),		\
-	/* */
+	FN(task_storage_get),		\
+	FN(task_storage_delete),	\
+	FN(get_current_task_btf),	\
+	FN(for_each_map_elem),		\	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
  * function eBPF program intends to call